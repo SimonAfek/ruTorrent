53e00952f0384d0b4bbd83b8fb81079716dbf66d
Simon Afek <simonaj92@gmail.com>
2023-11-10 13:13:10 +0100

    addtorrent.php tweaks

--- php/addtorrent.php
+++ php/addtorrent.php
@@ -99,10 +99,19 @@ else
 			}
 		}
 	}
-	if (!empty($_SERVER['PHP_SELF']) && !empty($_SERVER['HTTP_HOST']))
-		$location = "Location: //".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/addtorrent.php?";
-	else
-		$location = "Location: ./addtorrent.php?";
+	
+	$location = '';
+
+    if (!empty($_SERVER['HTTP_HOST'])) {
+        if ($_SERVER['HTTP_HOST'] == $_SERVER['HTTP_X_HOST']) {
+            $location = "Location: //".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/addtorrent.php?";
+        } else {
+            $location = "Location: //".$_SERVER['HTTP_X_HOST'].'/'.$_SERVER['USER'].dirname($_SERVER['PHP_SELF'])."/addtorrent.php?";
+        }
+    } else {
+        $location = "Location: ./addtorrent.php?";
+	}
+	
 	if(empty($uploaded_files))
 		$uploaded_files = array( array( 'status' => "Failed" ) );
 	foreach($uploaded_files as &$file)
